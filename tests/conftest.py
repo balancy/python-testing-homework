f3fe8efa731066e7803524e985f5147da481b309
balancy <balancy@gmail.com>
2023-09-26 12:00:25 +0200

    Remove unused code

--- tests/conftest.py
+++ tests/conftest.py
@@ -22,27 +22,3 @@ pytest_plugins = [
     'plugins.identity.external_api',
     'plugins.pictures.favourite_picture',
 ]
-
-
-def pytest_configure(config: pytest.Config) -> None:
-    """Configuring seed based on the command line option.
-
-    Options:
-        default: use default random seed
-        last: use the last used random seed
-    """
-    seed_value = config.getoption('randomly_seed')
-    default_seed = random.Random().getrandbits(32)
-
-    if seed_value == 'last':
-        seed = cast(pytest.Cache, config.cache).get(
-            'randomly_seed',
-            default_seed,
-        )
-    elif seed_value == 'default':
-        seed = default_seed
-    else:
-        seed = seed_value
-
-    cast(pytest.Cache, config.cache).set('randomly_seed', seed)
-    config.option.randomly_seed = seed
